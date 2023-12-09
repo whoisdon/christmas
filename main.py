2085084a2ed0fb6d85580f51dace64dce689e1af
whoisdon <conta.discordbaby@gmail.com>
2023-12-09 16:12:47 -0300

    feat: update imports from class

---
+++ main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+from pages.developer import Dev
+from pages.graphics import Graphics
+from pyspark.sql import SparkSession
+from pages.introduction import Introduction
+
+st.set_page_config(
+    page_title="Rinha de backend",
+    page_icon="🎅",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+pages = ['Introdução', 'Gráficos', 'Opções do Desenvolvedor']
+page = st.sidebar.selectbox('Guia', pages)
+
+spark = SparkSession.builder.appName('Rinha').getOrCreate()
+df = spark.read.csv('spotify.csv', header=True, inferSchema=True)
+
+if page == 'Introdução':
+    introduction = Introduction(spark, df)
+    introduction.display()
+elif page == 'Gráficos':
+    graphics = Graphics(spark, df)
+    graphics.display()
+elif page == 'Opções do Desenvolvedor':
+    dev = Dev(spark, df)
+    dev.display()
+    
